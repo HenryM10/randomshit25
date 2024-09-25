f4c32c44a523856f13308b6e5908bed098544f3a
HenryM10 <henrymeltsov002gmail.com>
2024-09-25 22:42:34 +0300

    did stuff

--- javascript/main.ts
+++ javascript/main.ts
@@ -2,11 +2,10 @@ const loginButton = document.querySelector('.login-button') as HTMLButtonElement
 const inputs = document.querySelectorAll('.text-input') as NodeListOf<HTMLInputElement>;
 const unfilledboxes = document.getElementById('error1');
 
-
-
 loginButton?.addEventListener('click', function() {
 
     const anyEmpty = Array.from(inputs).some(input => input.value.trim() === '');
+    
     if (anyEmpty) {
         if (unfilledboxes) {
             unfilledboxes.innerText = "Please fill in all the gaps!";
@@ -15,6 +14,6 @@ loginButton?.addEventListener('click', function() {
         if (unfilledboxes) {
             unfilledboxes.innerText = "";
         }
-        window.location.replace("index.html");
+        window.location.replace("index.html"); 
     }
 });
